5ff88de864c3df32d1fb89d6cbe7d8e4844a6d69
Arnaud Raoux <arnaud.raoux@normalesup.org>
2019-10-01 15:42:12 +0200

    Modification et simplification de la page recherche.

--- ressources/urls.py
+++ ressources/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.recherche_accueil, name="recherche_accueil"),
+    #path('', views.recherche_accueil, name="recherche_accueil"),
+    path('', views.recherche_tous, name="recherche_accueil"),
     path('recherche_categorie/', views.recherche_categorie, name="recherche_categorie"),
     path('recherche_oral/', views.recherche_oral, name="recherche_oral"),
     path('recherche_tous/', views.recherche_tous, name="recherche_tous"),
