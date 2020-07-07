8cc6923c6008ee684ba1965a4b5cee7def936e88
Arnaud Raoux <arnaud.raoux@sorbonne-universite.fr>
2020-07-07 22:45:08 +0200

    MaJ liens

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 
 
-lien_SU_master = "http://www.master.physique.upmc.fr/fr/m2/preparation-a-l-agregation-de-physique.html"
+lien_SU_master = "http://master.physique.sorbonne-universite.fr/fr/m1/parcours_m1/agregation.html"
 lien_P11_magistere = "http://www.u-psud.fr/fr/formations/diplomes/magisteres/magistere_de_physique_fondamentale_d_orsay.html"
 site_agreg_ext = "http://agregation-physique.org/"
 site_agreg_spe = "http://site2.agregation-physique.org/"
-programme_officiel_ext = "http://cache.media.education.gouv.fr/file/agregation_externe/55/4/p2017_agreg_ext_physchim_physique_556554.pdf"
-programme_officiel_spe = ""
+programme_officiel_ext = "https://media.devenirenseignant.gouv.fr/file/agreg_externe/69/2/p2020_agreg_ext_physchim_physique_1107692.pdf"
+programme_officiel_spe = "https://media.devenirenseignant.gouv.fr/file/agreg_externe_spec/65/0/p2020_agreg_ext_spe_phys_chimie_phys_1136650.pdf"
 
 lien_resultats_centre = "https://agreg.phys.ens.fr/#resultats"
