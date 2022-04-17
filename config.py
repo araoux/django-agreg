7ca512d83114c40b2e590934eca2334e0e54916e
Arnaud Raoux <arnaud.raoux@sorbonne-universite.fr>
2022-04-17 14:27:38 +0200

    MaJ descriptions

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 lien_SU_master = "http://master.physique.sorbonne-universite.fr/fr/m1/parcours_m1/agregation.html"
 lien_P11_magistere = "http://www.u-psud.fr/fr/formations/diplomes/magisteres/magistere_de_physique_fondamentale_d_orsay.html"
 site_agreg_ext = "http://agregation-physique.org/"
-site_agreg_spe = "http://site2.agregation-physique.org/"
+site_agreg_spe = "https://docteurs.agregation-physique.org/"
 programme_officiel_ext = "https://media.devenirenseignant.gouv.fr/file/agreg_externe/69/2/p2020_agreg_ext_physchim_physique_1107692.pdf"
 programme_officiel_spe = "https://media.devenirenseignant.gouv.fr/file/agreg_externe_spec/65/0/p2020_agreg_ext_spe_phys_chimie_phys_1136650.pdf"
 
